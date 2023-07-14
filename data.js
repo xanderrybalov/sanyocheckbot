e9a32182adbcb14eb7bfa28038ba6e1547afb6e3
Oleksandr Xander Rybalov <xanderrybalov@gmail.com>
2023-07-14 15:24:28 +0300

    add http

---
+++ data.js
@@ -0,0 +1,7 @@
+const comments = [
+    { id: 100, text: 'TOP MAN', author: 'Xander' },
+    { id: 101, text: 'Top Witch', author: 'Xandaryona' },
+    { id: 102, text: 'Top Knight', author: 'Sander' },
+];
+
+module.exports = comments;
---
+++ handlers.js
@@ -0,0 +1,97 @@
+const fs = require('fs');
+const qs = require('querystring');
+const comments = require('./data');
+
+function getHome(req, res) {
+    fs.readFile('./files/comment-form.html', (err, data) => {
+        if (err) {
+            res.statusCode = 500;
+            res.setHeader('Content-Type', 'text/html');
+            res.end('Server error while loading HTML file');
+        } else {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'text/html');
+            res.end(data);
+        }
+    });
+}
+function getHTML(req, res) {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html><body><div>');
+    res.write('<h1>Greetings from my HTTP server!</h1>');
+    res.write('</div></body></html>');
+    res.end();
+}
+
+function getText(req, res) {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('This is plain text');
+}
+
+function getComments(req, res) {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(comments));
+}
+
+function postComment(req, res) {
+    res.setHeader('Content-Type', 'text/plain');
+
+    if (req.headers['content-type'] === 'application/x-www-form-urlencoded') {
+        let body = '';
+
+        req.on('data', (chunk) => {
+            body += chunk.toString();
+        });
+        req.on('end', () => {
+            try {
+                const comment = qs.parse(body);
+                comment.id = parseInt(comment.id);
+                comments.push(comment);
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'text/html');
+                res.write('<h1>Command data was received</h1>');
+                res.write('<a href="/">Submit one more comment</a>');
+                res.end();
+            } catch (error) {
+                res.statusCode = 400;
+                res.end('Invalid Form data');
+            }
+        });
+    } else if (req.headers['content-type'] === 'application/json') {
+        let commentJSON = '';
+
+        req.on('data', (chunk) => (commentJSON += chunk));
+
+        req.on('end', () => {
+            try {
+                comments.push(JSON.parse(commentJSON));
+                res.statusCode = 200;
+                res.end('Command data was received');
+            } catch (error) {
+                res.statusCode = 400;
+                res.end('Invalid JSON');
+            }
+        });
+    } else {
+        res.statusCode = 400;
+        res.end('Data must be in the JSON format');
+    }
+}
+
+function handleNotFound(req, res) {
+    res.statusCode = 404;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('SORRY THIS PAGE IS NOT FOUND! KEKEKEKE');
+}
+
+module.exports = {
+    getHome,
+    getHTML,
+    getText,
+    getComments,
+    postComment,
+    handleNotFound,
+};
